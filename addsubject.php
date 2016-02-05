108ad350a66b2e7ae50a3329413101202a70d1b7
jonasanmaru <jonasanmaru@gmail.com>
2016-02-06 02:24:04 +0800

    add and drop subject php codes

---
+++ addsubject.php
@@ -0,0 +1,40 @@
+<?php
+  session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset = "utf-8">
+    <title>Adding Subject</title>
+  </head>
+
+  <body>
+
+<?php
+
+  $addsub = $_POST['addsub'];
+  $query = "";
+  //echo addsub;
+  if (!($database = mysql_connect("localhost", "root")))
+    die(mysql_error() . "<br>Could not connect to database</body></html>");
+
+  if (!mysql_select_db("twt", $database))
+    die(mysql_error() . "<br>Could not open database</body></html>");
+
+  if (!($result = mysql_query($query, $database))) {
+    print("<p> add not successful </p>");
+    die("</body></html>");
+  }
+
+  mysql_close($database);
+
+  print("<p> add successful </p>");
+?>
+
+
+
+ </body>
+
+</html>
