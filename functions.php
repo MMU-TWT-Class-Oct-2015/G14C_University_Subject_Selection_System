3104fd4c720c290c86e9227495850f44aa6ef4e3
NicL <lastnxs@gmail.com>
2016-02-06 15:57:26 +0800

    new function: sqlCon (take query strings & return result)

--- functions.php
+++ functions.php
@@ -31,4 +31,22 @@
       else
         return false;
   }
+  
+  function sqlCon($query) {
+
+    if (!($database = mysql_connect("localhost", "root")))
+      die(mysql_error() . "<br>Could not connect to database</body></html>");
+
+    if (!mysql_select_db("twt", $database))
+      die(mysql_error() . "<br>Could not open database</body></html>");
+
+    if (!($result = mysql_query($query, $database))) {
+      print("<p>User ID not found!</p>");
+      die("</body></html>");
+    }
+
+    mysql_close($database);
+
+    return $result;
+  }
  ?>
