13b517807320b58377726dafa25100836e51ef3d
Nic-chan <lastnxs@gmail.com>
2016-02-14 04:00:56 +0800

    modified drop_process to match with format of add_process, deleted functions.php and merged login function into login_process.php

--- add_process.php
+++ add_process.php
@@ -5,18 +5,18 @@
   if (mysqli_connect_errno())
     printf("<p style=color:red>Connection failed: ", mysqli_connect_error());
 
-  foreach($_POST['List'] as $subjectID) {
+  foreach($_POST['List'] as $SubjectID) {
     if (!$sth = $database->prepare("INSERT INTO student_subject (StudentID, SubjectID) VALUES (?,?)")) // prepare statement
-      header('Location: ./add_subject.php?error=1');
+      header("location: ./add_subject.php?error=1");
 
-    $sth->bind_Param('ss', $_SESSION['id'], $subjectID); // bind_param is used for modify records
+    $sth->bind_Param("ss", $_SESSION['id'], $SubjectID); // bind_param is used for modify records
 
     if (!$sth->execute())
-      header('Location: ./add_subject.php?error=1');
+      header("location: ./add_subject.php?error=1");
 
     $sth->close();
   }
   $database->close();
 
-  header('Location: ./index.php?success=1');
+  header("location: ./index.php?add=1");
 ?>
--- login_process.php
+++ login_process.php
@@ -1,10 +1,29 @@
 <?php
-  include_once 'functions.php';
-
   session_start();
 
-    if (login($_POST['userid'], $_POST['passw'], $mysqli))
-        header('Location: ./index.php');
-    else
-        header('Location: ./login.php?error=1');
+  $id = $_POST['userid'];
+  $password = $_POST['passw'];
+
+  // establish connection + error handling
+  $database = new mysqli("localhost","root","","twt");
+  if (mysqli_connect_errno())
+    printf("<p style=color:red>Connection to database failed: ", mysqli_connect_error());
+
+  if (!$sth = $database->prepare("SELECT Name, Password FROM student WHERE id = " . $id . " LIMIT 1")) // if user doesn't exists
+    header("location: ./login.php?error=1");
+
+  $sth->execute();
+  $sth->bind_result($Name,$hashedPw);
+  $sth->fetch();
+
+  $password = hash("sha512", $password); // hash entered password
+
+  if ($hashedPw == $password) { // compares both hashed password
+    // session variables storing info. of users
+    $_SESSION['id'] = $id;
+    $_SESSION['name'] = $Name;
+
+    header("location: ./index.php");
+  } else
+    header("location: ./login.php?error=1");
  ?>
