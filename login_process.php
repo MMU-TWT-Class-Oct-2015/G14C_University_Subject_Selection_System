020dbe49a24751048422dafaff772c63c39d16ae
NicL <lastnxs@gmail.com>
2016-02-14 22:01:52 +0800

    Update login_process.php

--- login_process.php
+++ login_process.php
@@ -1,13 +1,9 @@
 <?php
   session_start();
+  include './database_conn.php';
 
   $id = $_POST['userid'];
   $password = $_POST['passw'];
-
-  // establish connection + error handling
-  $database = new mysqli("localhost","root","","twt");
-  if (mysqli_connect_errno())
-    printf("<p style=color:red>Connection to database failed: ", mysqli_connect_error());
 
   if (!$sth = $database->prepare("SELECT Name, Password FROM student WHERE id = " . $id . " LIMIT 1")) // if user doesn't exists
     header("location: ./login.php?error=1");
