28a15495ea83afaa80a5fea3ebd15bc37d9346f1
NicL <lastnxs@gmail.com>
2016-02-15 14:21:59 +0800

    added password for gamma student and delta student
    234567
    345678

--- twt.sql
+++ twt.sql
@@ -39,8 +39,8 @@ CREATE TABLE `student` (
 
 INSERT INTO `student` (`ID`, `Name`, `Year`, `Password`) VALUES
 ('1234567890', 'Beta student', 1, 'ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413'),
-('2345678901', 'Gamma student', 2, ''),
-('3456789012', 'Delta Student', 3, '');
+('2345678901', 'Gamma student', 2, 'b920b68e218aed8c6cac6ba183816fd660079c2e2ec881a274efed5e771ace54e976c85c9ad5e771e3aa2c593c75b5dd5c607a92b5d36ce4fb64a7c42a2ea4bc'),
+('3456789012', 'Delta Student', 3, 'a24d59298bf15dcbcd568bff8e03222e8f2e491820489441c47c2422f62949b09cf5a6a105450ddfcbf6396e5e65cad48587bbdff9471bfecb7929a93544c876');
 
 -- --------------------------------------------------------
 
